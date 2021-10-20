fae8ba4fbd7fb5b15baadf7e2d264ff0d57a5615
Levenko Evgeny <levenko.evgeny@yandex.by>
2021-10-20 08:47:07 +0300

    rights

--- disease/views.py
+++ disease/views.py
@@ -2,24 +2,28 @@ from django.shortcuts import render
 from .models import Disease
 from .filters import DiseaseFilter
 from .forms import DiseaseForm
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.db import transaction
 from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy, reverse
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def disease_list(request):
-    path = str(request.get_full_path())
-    request.session['next_path'] = path
-    diseases_list = Disease.objects.all()
-    f = DiseaseFilter(request.GET, queryset=diseases_list)
-    return render(request, 'disease/disease_list.html', {
-        'diseases_list': f.qs,
-        'filter': f,
-    })
+    if request.user.is_superuser:
+        diseases_list = Disease.objects.all()
+        f = DiseaseFilter(request.GET, queryset=diseases_list)
+        return render(request, 'disease/disease_list.html', {
+            'diseases_list': f.qs,
+            'filter': f,
+        })
+    else:
+        return render(request, 'disease/not_enough_rights.html')
 
 
+@login_required
 def disease_input(request):
     if request.method == 'POST':
         form = DiseaseForm(request.POST)
@@ -36,6 +40,7 @@ def disease_input(request):
         return render(request, 'disease/disease_input_form.html', {'form': form, })
 
 
+@login_required
 def disease_update(request, disease_id):
     if request.method == 'POST':
         obj = get_object_or_404(Disease, pk=disease_id)
@@ -56,5 +61,6 @@ def disease_update(request, disease_id):
             'disease': disease,
         })
 
+@login_required
 def disease_delete(request):
     pass
